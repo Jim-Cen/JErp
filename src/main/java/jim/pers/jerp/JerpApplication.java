041b9e979cdfc605b841d4cae67eec45af98c79f
c_weking <c_weking@126.com>
2019-04-15 18:38:05 +0800

    mybatis

--- src/main/java/jim/pers/jerp/JerpApplication.java
+++ src/main/java/jim/pers/jerp/JerpApplication.java
@@ -1,9 +1,11 @@
 package jim.pers.jerp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("jim.pers.jerp.mapper")
 public class JerpApplication {
 
 	public static void main(String[] args) {
--- src/test/java/jim/pers/jerp/demo/HelloController.java
+++ src/test/java/jim/pers/jerp/demo/HelloController.java
@@ -1,5 +1,12 @@
 package jim.pers.jerp.demo;
 
+import jim.pers.jerp.mapper.UserMapper;
+import jim.pers.jerp.model.User;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -7,8 +14,23 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class HelloController {
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    SqlSessionFactory sqlSessionFactory;
+
+    @Autowired
+    SqlSession sqlSession;
+
     @GetMapping("/")
     public Mono<Person> hello() {
         return Mono.just(new Person(1,"Jim"));
+    }
+
+    @GetMapping("/User")
+    public  Mono<User> getUser(){ ;
+        User user=userMapper.getUserByName("Jim");
+        return  Mono.just(user);
     }
 }
