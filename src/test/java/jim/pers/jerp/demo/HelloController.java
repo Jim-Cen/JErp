515273f712bf726b13f9b66222d3a59c6b646689
c_weking <c_weking@126.com>
2019-04-20 20:44:11 +0800

    Session

--- src/test/java/jim/pers/jerp/demo/HelloController.java
+++ src/test/java/jim/pers/jerp/demo/HelloController.java
@@ -8,11 +8,23 @@ import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.core.io.buffer.DefaultDataBuffer;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebSession;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
 
 @RestController
 public class HelloController {
@@ -34,11 +46,26 @@ public class HelloController {
         return Mono.just(new Person(1,"Jim"));
     }
 
-    @AuthToken
-    @PostMapping("/User")
-    public  Mono<User> getUser(@RequestHeader("name") String name,@RequestBody Flux<String> path){
+    //@AuthToken
+    @RequestMapping("/User")
+    public  Mono<User> getUser(@RequestHeader("name") String name,
+                               @RequestBody Publisher<String> path,
+                               ServerHttpRequest request,
+                               ServerHttpResponse response,
+                               ServerWebExchange exchange,
+                               WebSession session){
         //authTokenAOP.authTokenCheck();
         //System.out.println(name);
+        //System.out.println(request.getId());
+        HttpHeaders headers = request.getHeaders();
+        //String headerName = headers.getFirst("name");
+        //System.out.println(headerName);
+        System.out.println(session.getId());
+//        if("jim".equals(headerName))
+                session.getAttributes().put("admin","jim");
+        String admin = session.getAttributes().get("admin").toString();
+        System.out.println(admin);
+        //DefaultDataBuffer dataBuffer= new DefaultDataBufferFactory().wrap("Hello\n\r".getBytes());
         User user=userMapper.getUserByName("Jim");
         return  Mono.just(user);
     }
