5a8493d449a5ab1b9fe0f8360d7cee0b725a5bc2
c_weking <c_weking@126.com>
2019-04-20 09:59:04 +0800

    Test

--- src/test/java/jim/pers/jerp/JerpApplicationTests.java
+++ src/test/java/jim/pers/jerp/JerpApplicationTests.java
@@ -1,59 +1,56 @@
 package jim.pers.jerp;
 
-import jim.pers.jerp.annotation.AuthToken;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-
-import java.net.URL;
+import org.springframework.web.reactive.function.BodyInserter;
+import reactor.core.publisher.Mono;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class JerpApplicationTests {
 
 	@Autowired
-	private TestRestTemplate restTemplate;
+	private WebTestClient webClient;
 
 	@LocalServerPort
 	private int port;
 
-	private URL base;
 
-	@AuthToken
-	public void aopTest(){
-
-	}
 	@Before
 	public void setUp() throws Exception {
 		String url = String.format("http://localhost:%d/", port);
 		System.out.println(String.format("port is : [%d]", port));
-		this.base = new URL(url);
 	}
 	@Test
 	public void test1() throws Exception {
-		//aopTest();
 
-		HttpHeaders requestHeaders = new HttpHeaders();
-		requestHeaders.add("name", "jim");
 		MultiValueMap<String, Object> postParameters = new LinkedMultiValueMap<>();
 		postParameters.add("path", "291974");
-		HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(postParameters, requestHeaders);
+		webClient.post()
+				   .uri("/User")
+					.header("name","jim")
+					.body(Mono.just(postParameters),MultiValueMap.class)
+					.exchange()
+					.expectBody()
+					.consumeWith(response -> {
+						byte[] body = response.getResponseBody();
+						System.out.println(new String(body));
+					});
+
 
 //		ResponseEntity<String> response = this.restTemplate.getForEntity(
 //				this.base.toString() + "/User", String.class, "");
-		ResponseEntity<String> response = this.restTemplate.exchange(this.base.toString() + "/User",
-										 HttpMethod.POST, requestEntity, String.class);
-		System.out.println(String.format("测试结果为：%s", response.getBody()));
+//		ResponseEntity<String> response = this.restTemplate.exchange(this.base.toString() + "/User",
+//										 HttpMethod.POST, requestEntity, String.class);
+		//System.out.println(String.format("测试结果为：%s", response.getBody()));
 	}
 }
