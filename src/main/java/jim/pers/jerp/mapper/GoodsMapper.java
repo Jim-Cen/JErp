7498a6eb468c324b57d4894212dd917419603ffe
c_weking <c_weking@126.com>
2019-05-10 04:37:37 +0800

    Goods

--- src/main/java/jim/pers/jerp/mapper/GoodsMapper.java
+++ src/main/java/jim/pers/jerp/mapper/GoodsMapper.java
@@ -2,14 +2,22 @@ package jim.pers.jerp.mapper;
 
 import jim.pers.jerp.model.Goods;
 import jim.pers.jerp.model.Unit;
+import org.apache.ibatis.annotations.MapKey;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface GoodsMapper {
+    public List<Goods> getAllGoods();
     public Goods getGoodsByUuid(int uuid);
     public List<Unit> getUnits();
-    public int setGoods(@Param("goods") Goods goods);
+
+    @MapKey("uuid")
+    public Map<Integer,Unit> getUnitsMap();
+
+    public int addGoods(Goods goods);
+    public int deleteGoods(int uuid);
+    public int updateGoods(Goods goods);
 }
